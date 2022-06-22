9d1493020adf7f70f092acb9c7976023f5b46b86
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2022-06-22 11:03:10 +0300

    Созданно Data и Repository

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList21.Models;
+
+namespace ToDoList21.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+              : base(options)
+        {
+
+        }
+        public DbSet<Problem> ProblemSet { get; set; }
+    }
+}
---
+++ Data/Repository/ProblemRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList21.Data.Repository
+{
+    public class ProblemRepository
+    {
+        private readonly AppDBContext _appDBContext;
+
+        public ProblemRepository(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+    }
+}
