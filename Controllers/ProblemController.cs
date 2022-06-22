cfedc85c8c8f85a210b33bf713602664d92ebb07
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2022-06-22 10:23:57 +0300

    создане Controllers

---
+++ Controllers/ProblemController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList21.Models;
+
+namespace ToDoList21.Controllers
+{
+    public class ProblemController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Create()
+        {
+            return View(new ProblemCreateViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(
+            [Bind("Title, Description, Executors, " +
+                    "PlannedComplexityTime")]
+                           ProblemCreateViewModel model
+            )
+        {
+            if (ModelState.IsValid)
+            {
+                var newProblem = new Problem
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    Executors = model.Executors,
+                    PlannedComplexityTime = model.PlannedComplexityTime
+                };
+
+                TempData["Message"] = "Задача " + newProblem.Title + " успешно создана!";
+
+                return RedirectToAction("Index");
+            }
+
+            TempData["Message"] = "Задача " + model.Title + " не может быть создана!";
+
+            return View(model);
+        }
+    }
+}
