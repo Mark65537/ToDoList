0df723ff8620d53855faaee3fca54043c6c1fdca
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2022-08-05 16:03:39 +0700

    изменение gulp для для автоматического изменения файла .css при сохранении файла .scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,22 @@
 /// <binding ProjectOpened='sass' />
 "use strict";
-//import gulpSass from "gulp-sass";
-//import nodeSass from "node-sass";
-
-//const sass = gulpSass(nodeSass);
 
 var gulp = require("gulp")
 const sass = require('gulp-sass')(require('sass'));
-/*    sass = require("gulp-sass"); // добавляем модуль sass*/
 
 var paths = {
     webroot: "./wwwroot/"
 };
+
 // регистрируем задачу для конвертации файла scss в css
 gulp.task("sass", function () {
     return gulp.src(paths.webroot+'Sass/site.scss')
         .pipe(sass())
         .pipe(gulp.dest(paths.webroot + '/css'));
 });
-
-//gulp.task('watch', function () {//Слежение за Sass-файлами
-//    gulp.watch(path.webroot +'Sass/site.scss', ['sass']);
-//    // Другие отслеживания
-//})
+gulp.watch(paths.webroot + 'Sass/site.scss', function () {    
+    return gulp.src(paths.webroot + 'Sass/site.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(paths.webroot + '/css'));
+    console.log('Sass changed');
+});
